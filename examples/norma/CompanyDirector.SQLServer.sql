9768eaf01e457ddaf2b8a08d9928d79ab47f7c41
Clifford Heath <clifford.heath@gmail.com>
2007-05-10 11:10:29 +0000

    Updated a couple of the example diagrams

--- examples/norma/CompanyDirector.SQLServer.sql
+++ examples/norma/CompanyDirector.SQLServer.sql
@@ -1,9 +1,9 @@
-﻿CREATE SCHEMA ManyToMany
+﻿CREATE SCHEMA CompanyDirector
 GO
 
 GO
 
-CREATE TABLE ManyToMany.DrctrAttnddBrdMtngOnDt
+CREATE TABLE CompanyDirector.Attendance
 (
 	"Date" BIGINT NOT NULL, 
 	Director_Person_Person_Name NATIONAL CHARACTER VARYING() NOT NULL, 
@@ -13,7 +13,7 @@ CREATE TABLE ManyToMany.DrctrAttnddBrdMtngOnDt
 GO
 
 
-CREATE TABLE ManyToMany.Director
+CREATE TABLE CompanyDirector.Director
 (
 	Person_Person_Name NATIONAL CHARACTER VARYING() NOT NULL, 
 	Company_Company_Name NATIONAL CHARACTER VARYING() NOT NULL, 
@@ -23,38 +23,38 @@ CREATE TABLE ManyToMany.Director
 GO
 
 
-ALTER TABLE ManyToMany.DrctrAttnddBrdMtngOnDt ADD CONSTRAINT DrctrAttnddBrdMtngOnDt_Director_FK FOREIGN KEY (Director_Person_Person_Name, Director_Company_Company_Name)  REFERENCES ManyToMany.Director (Person_Person_Name, Company_Company_Name)  ON DELETE NO ACTION ON UPDATE NO ACTION
+ALTER TABLE CompanyDirector.Attendance ADD CONSTRAINT Attendance_Director_FK FOREIGN KEY (Director_Person_Person_Name, Director_Company_Company_Name)  REFERENCES CompanyDirector.Director (Person_Person_Name, Company_Company_Name)  ON DELETE NO ACTION ON UPDATE NO ACTION
 GO
 
 
 
-CREATE PROCEDURE ManyToMany.InsrtDrctrAttnddBrdMtngOnDt
+CREATE PROCEDURE CompanyDirector.InsertAttendance
 (
 	@"Date" BIGINT , 
 	@Director_Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@Director_Company_Company_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	INSERT INTO ManyToMany.DrctrAttnddBrdMtngOnDt("Date", Director_Person_Person_Name, Director_Company_Company_Name)
+	INSERT INTO CompanyDirector.Attendance("Date", Director_Person_Person_Name, Director_Company_Company_Name)
 	VALUES (@"Date", @Director_Person_Person_Name, @Director_Company_Company_Name)
 GO
 
 
-CREATE PROCEDURE ManyToMany.DltDrctrAttnddBrdMtngOnDt
+CREATE PROCEDURE CompanyDirector.DeleteAttendance
 (
 	@"Date" BIGINT , 
 	@Director_Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@Director_Company_Company_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	DELETE FROM ManyToMany.DrctrAttnddBrdMtngOnDt
+	DELETE FROM CompanyDirector.Attendance
 	WHERE "Date" = @"Date" AND 
 Director_Person_Person_Name = @Director_Person_Person_Name AND 
 Director_Company_Company_Name = @Director_Company_Company_Name
 GO
 
 
-CREATE PROCEDURE ManyToMany."UpdtDrctrAttnddBrdMtngOnDt""Dt"""
+CREATE PROCEDURE CompanyDirector."UpdateAttendance""Date"""
 (
 	@"old_""Date""" BIGINT , 
 	@Old_Drctr_Prsn_Prsn_Nm NATIONAL CHARACTER VARYING() , 
@@ -62,7 +62,7 @@ CREATE PROCEDURE ManyToMany."UpdtDrctrAttnddBrdMtngOnDt""Dt"""
 	@"Date" BIGINT 
 )
 AS
-	UPDATE ManyToMany.DrctrAttnddBrdMtngOnDt
+	UPDATE CompanyDirector.Attendance
 SET "Date" = @"Date"
 	WHERE "Date" = @"old_""Date""" AND 
 Director_Person_Person_Name = @Old_Drctr_Prsn_Prsn_Nm AND 
@@ -70,7 +70,7 @@ Director_Company_Company_Name = @Old_Drctr_Cmpny_Cmpny_Nm
 GO
 
 
-CREATE PROCEDURE ManyToMany.UDABMODDPPN
+CREATE PROCEDURE CompanyDirector.UpdtAttndncDrctr_Prsn_Prsn_Nm
 (
 	@"old_""Date""" BIGINT , 
 	@Old_Drctr_Prsn_Prsn_Nm NATIONAL CHARACTER VARYING() , 
@@ -78,7 +78,7 @@ CREATE PROCEDURE ManyToMany.UDABMODDPPN
 	@Director_Person_Person_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	UPDATE ManyToMany.DrctrAttnddBrdMtngOnDt
+	UPDATE CompanyDirector.Attendance
 SET Director_Person_Person_Name = @Director_Person_Person_Name
 	WHERE "Date" = @"old_""Date""" AND 
 Director_Person_Person_Name = @Old_Drctr_Prsn_Prsn_Nm AND 
@@ -86,7 +86,7 @@ Director_Company_Company_Name = @Old_Drctr_Cmpny_Cmpny_Nm
 GO
 
 
-CREATE PROCEDURE ManyToMany.UDABMODDCCN
+CREATE PROCEDURE CompanyDirector.UADCCN
 (
 	@"old_""Date""" BIGINT , 
 	@Old_Drctr_Prsn_Prsn_Nm NATIONAL CHARACTER VARYING() , 
@@ -94,7 +94,7 @@ CREATE PROCEDURE ManyToMany.UDABMODDCCN
 	@Director_Company_Company_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	UPDATE ManyToMany.DrctrAttnddBrdMtngOnDt
+	UPDATE CompanyDirector.Attendance
 SET Director_Company_Company_Name = @Director_Company_Company_Name
 	WHERE "Date" = @"old_""Date""" AND 
 Director_Person_Person_Name = @Old_Drctr_Prsn_Prsn_Nm AND 
@@ -102,66 +102,66 @@ Director_Company_Company_Name = @Old_Drctr_Cmpny_Cmpny_Nm
 GO
 
 
-CREATE PROCEDURE ManyToMany.InsertDirector
+CREATE PROCEDURE CompanyDirector.InsertDirector
 (
 	@Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@Company_Company_Name NATIONAL CHARACTER VARYING() , 
 	@Appointed BIGINT 
 )
 AS
-	INSERT INTO ManyToMany.Director(Person_Person_Name, Company_Company_Name, Appointed)
+	INSERT INTO CompanyDirector.Director(Person_Person_Name, Company_Company_Name, Appointed)
 	VALUES (@Person_Person_Name, @Company_Company_Name, @Appointed)
 GO
 
 
-CREATE PROCEDURE ManyToMany.DeleteDirector
+CREATE PROCEDURE CompanyDirector.DeleteDirector
 (
 	@Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@Company_Company_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	DELETE FROM ManyToMany.Director
+	DELETE FROM CompanyDirector.Director
 	WHERE Person_Person_Name = @Person_Person_Name AND 
 Company_Company_Name = @Company_Company_Name
 GO
 
 
-CREATE PROCEDURE ManyToMany.UpdtDrctrPrsn_Prsn_Nm
+CREATE PROCEDURE CompanyDirector.UpdtDrctrPrsn_Prsn_Nm
 (
 	@old_Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@old_Company_Company_Name NATIONAL CHARACTER VARYING() , 
 	@Person_Person_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	UPDATE ManyToMany.Director
+	UPDATE CompanyDirector.Director
 SET Person_Person_Name = @Person_Person_Name
 	WHERE Person_Person_Name = @old_Person_Person_Name AND 
 Company_Company_Name = @old_Company_Company_Name
 GO
 
 
-CREATE PROCEDURE ManyToMany.UpdtDrctrCmpny_Cmpny_Nm
+CREATE PROCEDURE CompanyDirector.UpdtDrctrCmpny_Cmpny_Nm
 (
 	@old_Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@old_Company_Company_Name NATIONAL CHARACTER VARYING() , 
 	@Company_Company_Name NATIONAL CHARACTER VARYING() 
 )
 AS
-	UPDATE ManyToMany.Director
+	UPDATE CompanyDirector.Director
 SET Company_Company_Name = @Company_Company_Name
 	WHERE Person_Person_Name = @old_Person_Person_Name AND 
 Company_Company_Name = @old_Company_Company_Name
 GO
 
 
-CREATE PROCEDURE ManyToMany.UpdateDirectorAppointed
+CREATE PROCEDURE CompanyDirector.UpdateDirectorAppointed
 (
 	@old_Person_Person_Name NATIONAL CHARACTER VARYING() , 
 	@old_Company_Company_Name NATIONAL CHARACTER VARYING() , 
 	@Appointed BIGINT 
 )
 AS
-	UPDATE ManyToMany.Director
+	UPDATE CompanyDirector.Director
 SET Appointed = @Appointed
 	WHERE Person_Person_Name = @old_Person_Person_Name AND 
 Company_Company_Name = @old_Company_Company_Name
