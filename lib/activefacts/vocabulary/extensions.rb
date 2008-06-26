6e39239ffe9870622d05540b5879a26f9b25baac
Clifford Heath <clifford.heath@gmail.com>
2008-06-26 04:29:22 +0000

    Fixed error in finding preferred identifier, all valid CQL files processed successfully back to CQL

--- lib/activefacts/vocabulary/extensions.rb
+++ lib/activefacts/vocabulary/extensions.rb
@@ -72,21 +72,24 @@ module ActiveFacts
                   }
                 throw :pi, nil
               end
-            debug "Got PI #{pi.name} for nested #{name}" if pi
+            debug "Got PI #{pi.name||pi.object_id} for nested #{name}" if pi
             debug "Looking for PI on entity that nests this fact" unless pi
             raise "Oops, pi for nested fact is #{pi.class}" unless !pi || PresenceConstraint === pi
             return pi if pi
           end
         end
 
-        debug "\nLooking for PI for ordinary entity #{name} with #{all_role.size} roles:" do
+        debug "Looking for PI for ordinary entity #{name} with #{all_role.size} roles:" do
           debug "Roles are in fact types #{all_role.map{|r| r.fact_type.describe(r)}*", "}"
           pi = catch :pi do
               all_supertypes = supertypes_transitive
               debug "PI roles must be played by one of #{all_supertypes.map(&:name)*", "}" if all_supertypes.size > 1
               all_role.each{|role|
+                  next unless role.unique || fact_type
                   ftroles = role.fact_type.all_role
-                  next if ftroles.size > 2        # Skip roles in objectified fact types
+
+                  # Skip roles in ternary and higher fact types, they're objectified
+                  next if ftroles.size > 2
 
                   debug "Considering role in #{role.fact_type.describe(role)}"
 
@@ -95,29 +98,42 @@ module ActiveFacts
                   pi_role = ftroles.size == 1 || ftroles[1] == role ? ftroles[0] : ftroles[1]
 
                   next if ftroles.size == 2 && pi_role.concept == self
-                  debug "\tConsidering #{pi_role.concept.name} as a PI role"
+                  debug "  Considering #{pi_role.concept.name} as a PI role"
 
-                  # Look in all role sequences that include this related role
+                  # If this is an identifying role, the PI is a PC whose role_sequence spans the role.
+                  # Walk through all role_sequences that span this role, and test each:
                   pi_role.all_role_ref.each{|rr|
                       role_sequence = rr.role_sequence  # A role sequence that includes a possible role
-                      debug "\t\tConsidering role sequence #{role_sequence.describe}"
 
-                      # All roles in this role_sequence to fact types
-                      # which (apart from that role) only have roles
-                      # played by the original entity type or a supertype.
-                      next if role_sequence.all_role_ref.detect{|rsr|
-                          fact_type_roles = rsr.role.fact_type.all_role
+                      debug "    Considering role sequence #{role_sequence.describe}"
+
+                      # All roles in this role_sequence must be in fact types which
+                      # (apart from that role) only have roles played by the original
+                      # entity type or a supertype.
+                      #debug "      All supertypes #{all_supertypes.map{|st| "#{st.object_id}=>#{st.name}"}*", "}"
+                      if role_sequence.all_role_ref.detect{|rsr|
+                          fact_type = rsr.role.fact_type
+                          debug "      Role Sequence touches #{fact_type.describe(pi_role)}"
+
+                          fact_type_roles = fact_type.all_role
+                          debug "      residual is #{fact_type_roles.map{|r| r.concept.name}.inspect} minus #{rsr.role.concept.name}"
                           residual_roles = fact_type_roles-[rsr.role]
                           residual_roles.detect{|rfr|
-                              !all_supertypes.include?(rfr.concept)
+                              debug "        Checking residual role #{rfr.concept.object_id}=>#{rfr.concept.name}"
+# This next line looks right, but breaks things. Find out what and why:
+#                              !rfr.unique or
+                                !all_supertypes.include?(rfr.concept)
                             }
                         }
+                        debug "      Discounting this role_sequence because it includes alien roles"
+                        next
+                      end
 
                       # Any presence constraint over this role sequence is a candidate
                       rr.role_sequence.all_presence_constraint.detect{|pc|
                           # Found it!
                           if pc.is_preferred_identifier
-                            debug "found PI #{pc.name}, is_preferred_identifier=#{pc.is_preferred_identifier.inspect}, enforcement=#{pc.enforcement}"
+                            debug "found PI #{pc.name||pc.object_id}, is_preferred_identifier=#{pc.is_preferred_identifier.inspect} over #{pc.role_sequence.describe}"
                             throw :pi, pc
                           end
                         }
@@ -126,7 +142,7 @@ module ActiveFacts
               throw :pi, nil
             end
           raise "Oops, pi for entity is #{pi.class}" if pi && !(PresenceConstraint === pi)
-          debug "Got PI #{pi.name} for #{name}" if pi
+          debug "Got PI #{pi.name||pi.object_id} for #{name}" if pi
 
           if !pi
             if (supertype = identifying_supertype)
--- spec/cql_spec.rb
+++ spec/cql_spec.rb
@@ -46,4 +46,6 @@ describe "CQL Loader" do
       File.delete(actual_file)  # It succeeded, we don't need the file.
     end
   end
+
+  it "should handle role names in identifiers, with and without adjectives and duplicate role players"
 end
