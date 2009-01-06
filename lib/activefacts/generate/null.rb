c724bfd98250fec546743297539b1938099eda9b
Clifford Heath <clifford.heath@gmail.com>
2009-01-06 06:21:58 +0000

    Added some options

--- lib/activefacts/generate/null.rb
+++ lib/activefacts/generate/null.rb
@@ -4,14 +4,20 @@
 # Copyright (c) 2007 Clifford Heath. Read the LICENSE file.
 # Author: Clifford Heath.
 #
+require 'activefacts/persistence'
+
 module ActiveFacts
   module Generate
     class NULL
-      def initialize(vocabulary)
+      def initialize(vocabulary, *options)
         @vocabulary = vocabulary
+        @vocabulary = @vocabulary.Vocabulary.values[0] if ActiveFacts::API::Constellation === @vocabulary
+        @tables = options.include? "tables"
+        @columns = options.include? "columns"
       end
 
       def generate(out = $>)
+        @vocabulary.tables if @tables || @columns
       end
     end
   end
