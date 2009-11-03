c0199c57d00b6f017f9e97d3e21b9d8eaee3b602
Clifford Heath <clifford.heath@gmail.com>
2009-11-03 08:57:48 +0000

    Refactored compiler tests to (mostly) use blocks

--- spec/cql/matching_spec.rb
+++ spec/cql/matching_spec.rb
@@ -6,7 +6,7 @@
 require 'activefacts/support'
 require 'activefacts/api/support'
 require 'activefacts/cql/compiler'
-# require File.dirname(__FILE__) + '/../helpers/test_parser'
+# require File.dirname(__FILE__) + '/../helpers/compiler_helper'  # Can't see how to include/extend these methods correctly
 
 describe "Fact Type Role Matching" do
   Prefix = %q{
@@ -16,17 +16,35 @@ describe "Fact Type Role Matching" do
   }
   BaseConcepts = 3  # String, Boy, Girl
 
-  def self.SingleFact
+  def self.SingleFact &b
     lambda {|c|
       c.FactType.size.should == 1
       @fact_type = c.FactType.values[0]
+      b.call(@fact_type) if b
+      @fact_type
     }
+  end
+
+  def self.PresenceConstraints fact_type, &b
+    @presence_constraints =
+      fact_type.all_role.map{|r|
+        r.all_role_ref.map{|rr|
+          rr.role_sequence.all_presence_constraint.to_a
+        }
+      }.flatten.uniq
+    b.call(@presence_constraints) if b
+    @presence_constraints
+  end
+
+  def self.Readings fact_type, &b
+    @readings = fact_type.all_reading.sort_by{|r| r.ordinal}
+    b.call(@readings) if b
+    @readings
   end
 
   def self.ReadingCount n
     lambda {|c|
       unless @fact_type.all_reading.size == n
-        # REVISIT: Figure out how to suppress this inside a pending block
         puts "SPEC FAILED, wrong number of readings (should be #{n}):\n\t#{
           @fact_type.all_reading.map{ |r| r.expand}*"\n\t"
         }"
@@ -96,11 +114,11 @@ describe "Fact Type Role Matching" do
   end
 
   def self.pending(msg = "TODO", &b)
-    lambda {|c|
+    lambda {|*c|
       raised = nil
       begin
         example = b.call
-        eval(lambda { example.call(c) }, BlackHole.new)
+        eval(lambda { example.call(*c) }, BlackHole.new)
       rescue => raised
       end
       raise Spec::Example::PendingExampleFixedError.new(msg) unless raised
@@ -122,15 +140,19 @@ describe "Fact Type Role Matching" do
   SimpleBinaryFactTypeTests = [
     [ # Simple create
       %q{Girl is going out with at most one Boy; },
-      SingleFact(),
-        ReadingCount(1),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 1
+        PresenceConstraints(fact_type) do |pcs|
+          pcs.size.should == 1
+        end
+      end
     ],
     [ # Create with explicit adjective
       %q{Girl is going out with at most one ugly-Boy;},
-      SingleFact(),
-        ReadingCount(1),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 1
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Simple match
       %q{Girl is going out with at most one Boy; },
@@ -138,9 +160,10 @@ describe "Fact Type Role Matching" do
         Girl is going out with Boy,
           Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Simple match with repetition
       %q{Girl is going out with at most one Boy; },
@@ -150,11 +173,15 @@ describe "Fact Type Role Matching" do
           Boy is going out with Girl,
           Boy is going out with Girl;
       },
-      SingleFact(),
-        PresenceConstraintCount(1),
+      SingleFact() do |fact_type|
+        PresenceConstraints(fact_type).size.should == 1
         pending("duplicate new clauses are not eliminated") do
-          ReadingCount(2)
-        end
+          Readings(fact_type).size.should == 2
+        end.call      # Must call the pending block
+      end,
+#      pending("duplicate new clauses are not eliminated") do
+#        @readings.size.should == 2
+#      end
     ],
     [ # Simple match with a new presence Constraint
       %q{Girl is going out with at most one Boy; },
@@ -162,9 +189,10 @@ describe "Fact Type Role Matching" do
         Girl is going out with Boy,
           Boy is going out with at most one Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(2)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 2
+      end
     ],
     [ # RoleName matching
       %q{Girl is going out with at most one Boy;},
@@ -172,54 +200,60 @@ describe "Fact Type Role Matching" do
         Boy is going out with Girlfriend,
           Girl (as Girlfriend) is going out with at most one Boy;
       },
-      SingleFact(),
-        ReadingCount(3),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 3
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with explicit adjective
       %q{Girl is going out with at most one ugly-Boy;},
       %q{Girl is going out with at most one ugly-Boy,
         ugly-Boy is best friend of Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with implicit adjective
       %q{Girl is going out with at most one ugly-Boy;},
       %q{Girl is going out with ugly Boy,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with explicit trailing adjective
       %q{Girl is going out with at most one Boy-monster;},
       %q{Girl is going out with Boy-monster,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with implicit trailing adjective
       %q{Girl is going out with at most one Boy-monster;},
       %q{Girl is going out with Boy monster,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with two explicit adjectives
       %q{Girl is going out with at most one ugly- bad Boy;},
       %q{Girl is going out with ugly- bad Boy,
         ugly- bad Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with two implicit adjective
       %q{Girl is going out with at most one ugly- bad Boy;},
@@ -235,18 +269,20 @@ describe "Fact Type Role Matching" do
       %q{Girl is going out with Boy real -monster,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with two implicit trailing adjectives
       %q{Girl is going out with at most one Boy real -monster;},
       %q{Girl is going out with Boy real monster,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with hyphenated word
       %q{Girl is going out with at most one Boy; },
@@ -254,19 +290,23 @@ describe "Fact Type Role Matching" do
         Girl is going out with Boy,
           Boy is out driving a semi-trailer with Girl;
       },
-      SingleFact(),
-        ReadingCount(2),
-        ReadingContainsHyphenatedWord(1),
+      SingleFact() do |fact_type|
+        (readings = Readings(fact_type)).size.should == 2
+        ## REVISIT: Refactor test
+        #ReadingContainsHyphenatedWord(readings[1])
+        ReadingContainsHyphenatedWord(1)
         PresenceConstraintCount(1)
+      end
     ],
     [ # Match with implicit leading ignoring explicit trailing adjective
       %q{Girl is going out with at most one ugly-Boy;},
       %q{Girl is going out with ugly Boy-monster,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(3),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 3
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with implicit leading ignoring implicit trailing adjective
       %q{Girl is going out with at most one ugly-Boy;},
@@ -282,18 +322,20 @@ describe "Fact Type Role Matching" do
       %q{Girl is going out with ugly-Boy monster,
         Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(3),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 3
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
     [ # Match with implicit trailing ignoring implicit leading adjective
       %q{Girl is going out with at most one Boy-monster;},
       %q{Girl is going out with ugly Boy monster,
         ugly-Boy is going out with Girl;
       },
-      SingleFact(),
-        ReadingCount(3),
-        PresenceConstraintCount(1)
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 3
+        PresenceConstraints(fact_type).size.should == 1
+      end
     ],
   ]
 
@@ -302,10 +344,12 @@ describe "Fact Type Role Matching" do
       # REVISIT: At present, this doesn't add the minimum frequency constraint that a preferred identifier requires.
       %q{Thong is written as String;},
       %q{Thing is identified by Thong where Thing has one Thong;},
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 1
+        PresenceConstraints(fact_type).size.should == 2
+      end,
       Concept('Thong'),
         WrittenAs('String'),
-      SingleFact(),
-        ReadingCount(1),
       ConceptCount(2+BaseConcepts),
       Concept('Thing'),
         PreferredIdentifier(1),
@@ -314,8 +358,10 @@ describe "Fact Type Role Matching" do
     [
       %q{Thong is written as String;},
       %q{Thing is identified by Thong where Thing has one Thong, Thong is of one Thing;},
-      SingleFact(),
-        ReadingCount(2),
+      SingleFact() do |fact_type|
+        Readings(fact_type).size.should == 2
+        PresenceConstraints(fact_type).size.should == 2
+      end,
       ConceptCount(2+BaseConcepts),
       Concept('Thing'),
         PreferredIdentifier(1),
