de1be755dc5fe9f71e8f1ce769d9b27cbadfbbda
Clifford Heath <clifford.heath@gmail.com>
2010-10-20 15:06:56 +1100

    Updated diagram load and dump scripts for SQL 2005

--- examples/SQL/DumpDiagrams.sh
+++ examples/SQL/DumpDiagrams.sh
@@ -1,2 +1,4 @@
-bcp Metamodel..dtproperties out Metamodel.bcp -c -T -S localhost
+REM bcp Metamodel..dtproperties out Metamodel.bcp -c -T -S localhost
+
+bcp FBM_ORM_MODEL_20100824.dbo.sysdiagrams out Metamodel.bcp -c -T -S localhost
 
--- examples/SQL/LoadDiagrams.sh
+++ examples/SQL/LoadDiagrams.sh
@@ -1,1 +1,3 @@
-bcp Metamodel..dtproperties in Metamodel.bcp -c -T -S localhost
+rem bcp Metamodel..dtproperties in Metamodel.bcp -c -T -S localhost
+
+bcp FBM_ORM_MODEL_20100831.dbo.sysdiagrams in FBM_ORM_MODEL_20100831.bcp -c -T -S localhost
