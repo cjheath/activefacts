0ac58bfc04df4df287142649f3af800b4c72854e
Clifford Heath <clifford.heath@gmail.com>
2010-11-04 11:21:33 +1100

    Updated generated files for Diplomacy model

--- examples/SQL/Diplomacy.sql
+++ examples/SQL/Diplomacy.sql
@@ -1,3 +1,10 @@
+CREATE TABLE Country (
+	-- Country has CountryName,
+	CountryName                             varchar NOT NULL,
+	PRIMARY KEY(CountryName)
+)
+GO
+
 CREATE TABLE Diplomat (
 	-- Diplomat has DiplomatName,
 	DiplomatName                            varchar NOT NULL,
@@ -5,7 +12,9 @@ CREATE TABLE Diplomat (
 	RepresentedCountryName                  varchar NOT NULL,
 	-- Diplomat serves in Country (as Served Country) and Country has CountryName,
 	ServedCountryName                       varchar NOT NULL,
-	PRIMARY KEY(DiplomatName)
+	PRIMARY KEY(DiplomatName),
+	FOREIGN KEY (RepresentedCountryName) REFERENCES Country (CountryName),
+	FOREIGN KEY (ServedCountryName) REFERENCES Country (CountryName)
 )
 GO
 
@@ -19,12 +28,21 @@ CREATE TABLE Fluency (
 )
 GO
 
+CREATE TABLE Language (
+	-- Language has LanguageName,
+	LanguageName                            varchar NOT NULL,
+	PRIMARY KEY(LanguageName)
+)
+GO
+
 CREATE TABLE LanguageUse (
 	-- LanguageUse is where Language is spoken in Country and Country has CountryName,
 	CountryName                             varchar NOT NULL,
 	-- LanguageUse is where Language is spoken in Country and Language has LanguageName,
 	LanguageName                            varchar NOT NULL,
-	PRIMARY KEY(LanguageName, CountryName)
+	PRIMARY KEY(LanguageName, CountryName),
+	FOREIGN KEY (CountryName) REFERENCES Country (CountryName),
+	FOREIGN KEY (LanguageName) REFERENCES Language (LanguageName)
 )
 GO
 
@@ -36,7 +54,13 @@ CREATE TABLE Representation (
 	-- Representation is where Ambassador is from Country (as Represented Country) to Country and Country has CountryName,
 	RepresentedCountryName                  varchar NOT NULL,
 	PRIMARY KEY(RepresentedCountryName, CountryName),
+	FOREIGN KEY (CountryName) REFERENCES Country (CountryName),
+	FOREIGN KEY (RepresentedCountryName) REFERENCES Country (CountryName),
 	FOREIGN KEY (AmbassadorName) REFERENCES Diplomat (DiplomatName)
 )
+GO
+
+ALTER TABLE Fluency
+	ADD FOREIGN KEY (LanguageName) REFERENCES Language (LanguageName)
 GO
 
