1e942a25306fee807fb0aef212a47c125be86fd2
Clifford Heath <clifford.heath@gmail.com>
2012-08-07 16:17:50 +1000

    The ServiceDirector example had been getting away with some illegal structures

--- examples/ruby/ServiceDirector.rb
+++ examples/ruby/ServiceDirector.rb
@@ -147,7 +147,7 @@ module ::ServiceDirector
     one_to_one :credential_nr, :mandatory => true  # See CredentialNr.credential
     has_one :data_store                         # See DataStore.all_credential
     has_one :data_store_service                 # See DataStoreService.all_credential
-    has_one :expiration_date, :class => "Date"  # See Date.all_credential_as_expiration_date
+    has_one :expiration_date_yymmdd, :class => "DateYYMMDD"  # See DateYYMMDD.all_credential_as_expiration_date_yymmdd
     has_one :password, :mandatory => true       # See Password.all_credential
     has_one :user_name, :mandatory => true      # See UserName.all_credential
     has_one :vendor                             # See Vendor.all_credential
@@ -168,14 +168,14 @@ module ::ServiceDirector
     has_one :secondary_host_system, :class => "HostSystem", :mandatory => true  # See HostSystem.all_data_store_as_secondary_host_system
   end
 
-  class Date
-    identified_by :ddmmyyyy
-    one_to_one :ddmmyyyy, :class => DDMMYYYY, :mandatory => true  # See DDMMYYYY.date
+  class DateTimeYMDHMS
+    identified_by :mdyhms
+    one_to_one :mdyhms, :class => MDYHMS, :mandatory => true  # See MDYHMS.date_time_ymdhms
   end
 
-  class DateTime
-    identified_by :mdyhms
-    one_to_one :mdyhms, :class => MDYHMS, :mandatory => true  # See MDYHMS.date_time
+  class DateYYMMDD
+    identified_by :ddmmyyyy
+    one_to_one :ddmmyyyy, :class => DDMMYYYY, :mandatory => true  # See DDMMYYYY.date_yymmdd
   end
 
   class Duration
@@ -249,7 +249,7 @@ module ::ServiceDirector
     has_one :monitor, :counterpart => :all_exclusion_recurring_schedule  # See Monitor.all_all_exclusion_recurring_schedule
     one_to_one :recurring_schedule_id, :mandatory => true  # See RecurringScheduleId.recurring_schedule
     maybe :saturday
-    has_one :start_time, :class => "Time", :mandatory => true  # See Time.all_recurring_schedule_as_start_time
+    has_one :start_time_hms, :class => "TimeHMS", :mandatory => true  # See TimeHMS.all_recurring_schedule_as_start_time_hms
     maybe :sunday
     maybe :thursday
     maybe :tuesday
@@ -260,7 +260,7 @@ module ::ServiceDirector
     identified_by :satellite_message_id
     has_one :data_store, :mandatory => true     # See DataStore.all_satellite_message
     has_one :group_transaction, :class => "Transaction"  # See Transaction.all_satellite_message_as_group_transaction
-    has_one :insertion_date_time, :class => DateTime, :mandatory => true  # See DateTime.all_satellite_message_as_insertion_date_time
+    has_one :insertion_date_time_ymdhms, :class => DateTimeYMDHMS, :mandatory => true  # See DateTimeYMDHMS.all_satellite_message_as_insertion_date_time_ymdhms
     has_one :message_data                       # See MessageData.all_satellite_message
     has_one :message_header                     # See MessageHeader.all_satellite_message
     has_one :provider_type                      # See ProviderType.all_satellite_message
@@ -270,9 +270,9 @@ module ::ServiceDirector
 
   class Subscription
     identified_by :subscription_nr
-    has_one :beginning_date, :class => Date, :mandatory => true  # See Date.all_subscription_as_beginning_date
+    has_one :beginning_date_yymmdd, :class => DateYYMMDD, :mandatory => true  # See DateYYMMDD.all_subscription_as_beginning_date_yymmdd
     one_to_one :company, :counterpart => :driver_tech_subscription  # See Company.driver_tech_subscription
-    has_one :ending_date, :class => Date        # See Date.all_subscription_as_ending_date
+    has_one :ending_date_yymmdd, :class => DateYYMMDD  # See DateYYMMDD.all_subscription_as_ending_date_yymmdd
     maybe :is_enabled
     one_to_one :subscription_nr, :mandatory => true  # See SubscriptionNr.subscription
   end
@@ -292,9 +292,9 @@ module ::ServiceDirector
     one_to_one :switch_id, :mandatory => true   # See SwitchId.switch
   end
 
-  class Time
+  class TimeHMS
     identified_by :hhmmss
-    one_to_one :hhmmss, :class => HHMMSS, :mandatory => true  # See HHMMSS.time
+    one_to_one :hhmmss, :class => HHMMSS, :mandatory => true  # See HHMMSS.time_hms
   end
 
   class Transaction
