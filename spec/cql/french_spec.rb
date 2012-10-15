ba2e4ee196a1a7d4c4b25b476f9276be38b7ac02
Clifford Heath <clifford.heath@gmail.com>
2012-10-15 11:38:11 +1100

    added missing french_spec

---
+++ spec/cql/french_spec.rb
@@ -0,0 +1,21 @@
+
+
+# Examples:
+#
+# Company has at most one postal-Address;
+# CompanyContact is where Company has contact-Person;
+# CompanyContact is the situation where Company has contact-Person;
+# for each Company at least one of the following holds:
+#     that Company has some contact-Person,
+#     that Company has some mailing-Address and that mailing-Address includes PostCode;
+#
+# Entreprise is a synonym for English:Company;
+# Entreprise est un synonyme pour English:Company;
+# Personne is a synonym for English:Person;
+#
+# Entreprise a au plus une Adresse Postale;
+# EntrepriseContact est quand un(e) Entreprise a Personnes comme -contact;
+# pour chaque Entreprise au moins un des suivants s'applique:
+#     que cette Entreprise a certaines Personnes comme -contact,
+#     que cette Entreprise a certaines Adresses Postales et que ces Adresses Postales ont un Code Postale;
+#
