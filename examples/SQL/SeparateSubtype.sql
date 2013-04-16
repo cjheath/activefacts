d02fd399d74b093dea1d198449ba3fff5899bfd8
Clifford Heath <clifford.heath@gmail.com>
2013-04-16 15:16:10 +1000

    Reworked an example to better show foreign keys between absorbed entities

--- examples/SQL/SeparateSubtype.sql
+++ examples/SQL/SeparateSubtype.sql
@@ -3,17 +3,31 @@ CREATE TABLE Claim (
 	ClaimID                                 int IDENTITY NOT NULL,
 	-- maybe Claim concerns Incident and maybe Incident occurred on Date Time,
 	IncidentDateTime                        datetime NULL,
+	-- maybe Claim concerns Incident and maybe Witness saw Incident and Person has Person Name,
+	IncidentWitnessName                     varchar NULL,
 	PRIMARY KEY(ClaimID)
 )
 GO
 
+CREATE TABLE Person (
+	-- Person has Person Name,
+	PersonName                              varchar NOT NULL,
+	PRIMARY KEY(PersonName)
+)
+GO
+
 CREATE TABLE VehicleIncident (
-	-- maybe Vehicle Incident occured while Driver was in charge and Driver has Driver Name,
+	-- maybe Vehicle Incident occured while Driver was in charge and Person has Person Name,
 	DriverName                              varchar NULL,
 	-- Vehicle Incident is a kind of Incident and Claim has Claim ID,
 	IncidentID                              int NOT NULL,
 	PRIMARY KEY(IncidentID),
-	FOREIGN KEY (IncidentID) REFERENCES Claim (ClaimID)
+	FOREIGN KEY (IncidentID) REFERENCES Claim (ClaimID),
+	FOREIGN KEY (DriverName) REFERENCES Person (PersonName)
 )
+GO
+
+ALTER TABLE Claim
+	ADD FOREIGN KEY (IncidentWitnessName) REFERENCES Person (PersonName)
 GO
 
--- examples/ruby/SeparateSubtype.rb
+++ examples/ruby/SeparateSubtype.rb
@@ -10,7 +10,7 @@ module ::SeparateSubtype
     value_type 
   end
 
-  class DriverName < String
+  class PersonName < String
     value_type 
   end
 
@@ -19,19 +19,26 @@ module ::SeparateSubtype
     one_to_one :claim_id, :class => ClaimID, :mandatory => true  # See ClaimID.claim
   end
 
-  class Driver
-    identified_by :driver_name
-    one_to_one :driver_name, :mandatory => true  # See DriverName.driver
-  end
-
   class Incident
     identified_by :claim
     one_to_one :claim, :mandatory => true       # See Claim.incident
     has_one :date_time                          # See DateTime.all_incident
+    has_one :witness                            # See Witness.all_incident
+  end
+
+  class Person
+    identified_by :person_name
+    one_to_one :person_name, :mandatory => true  # See PersonName.person
   end
 
   class VehicleIncident < Incident
     has_one :driver                             # See Driver.all_vehicle_incident
+  end
+
+  class Witness < Person
+  end
+
+  class Driver < Person
   end
 
 end
