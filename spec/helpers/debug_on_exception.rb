7d1566c3fd7d0d3db523555c7922d9d224732e54
Clifford Heath <clifford.heath@gmail.com>
2014-03-28 16:15:55 +1100

    In the first-chance exception catcher, ignore the exceptions generated inside Array#flatten

--- spec/helpers/debug_on_exception.rb
+++ spec/helpers/debug_on_exception.rb
@@ -7,6 +7,9 @@ if ENV["RSPEC_DEBUG_ON_EXCEPTION"]
     def initialize *args, &b
       send(:old_initialize, *args, &b)
 
+      # Array#flatten produces many spurious to_ary exceptions:
+      return if self.class == NoMethodError && args[0] =~ /\Aundefined method `to_ary' /
+
       if (e = ENV["RSPEC_DEBUG_ON_EXCEPTION"]).index('all') or
 	e.index(self.class.name.sub(/.*::/,''))
 	puts "Stopped due to #{self.class} at "+caller*"\n\t"
